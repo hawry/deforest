d25f9787b0f50c8ade497c9804bf6bcb975535c9
hawry <hawry@hawry.net>
2019-10-14 22:32:48 +0200

    add unit tests and travis file

---
+++ deforest/tests/test_cf_cleaner.py
@@ -0,0 +1,52 @@
+import deforest.cleaners as cleaner
+
+
+class TestCFCleaner:
+    result = None
+
+    def test_clean_no_cf(self):
+        self.result = {"swagger": "2.0", "info": {
+            "title": "This is a title", "version": "1.0"}, "x-amazon-apigateway-request-validators": {"all": {"validateRequestBody": True, "validateRequestParameters": True}}}
+        sut = cleaner.CloudFormationCleaner(self)
+        sut.clean()
+        assert "x-amazon-apigateway-request-validators" in self.result[0]
+
+    def test_clean_cf_removed(self):
+        self.result = {"AWSTemplateFormatVersion": "2010-09-09", "Description": "a sample template", "Resources": {"MyRestApi": {"Type": "AWS::ApiGateway::RestApi", "Properties": {
+            "Body": {
+                "swagger": "2.0", "info": {"title": "a title", "version": "0.1"}
+            }
+        }}}}
+        sut = cleaner.CloudFormationCleaner(self)
+        sut.clean()
+        assert "AWSTemplateFormatVersion" not in self.result[0]
+        assert "info" in self.result[0]
+        assert len(self.result) == 1
+
+    def test_clean_cf_two_apis(self):
+        self.result = {"AWSTemplateFormatVersion": "2010-09-09", "Description": "a sample template", "Resources": {"MyRestApi": {"Type": "AWS::ApiGateway::RestApi", "Properties": {
+            "Body": {
+                "swagger": "2.0", "info": {"title": "a title", "version": "0.1"}
+            }
+        }}, "MySecondsRestApi": {"Type": "AWS::ApiGateway::RestApi", "Properties": {"Body": {"swagger": "2.0", "info": {"title": "second api"}}}}}}
+        sut = cleaner.CloudFormationCleaner(self)
+        sut.clean()
+
+        assert len(self.result) == 2
+        assert "AWSTemplateFormatVersion" not in self.result[0]
+        assert "AWSTemplateFormatVersion" not in self.result[1]
+        assert "info" in self.result[0]
+        assert "swagger" in self.result[1]
+
+    def test_clean_cf_ignore_invalid_api(self):
+        self.result = {"AWSTemplateFormatVersion": "2010-09-09", "Description": "a sample template", "Resources": {"MyRestApi": {"Type": "AWS::ApiGateway::RestApi", "Properties": {
+            "Body": {
+                "swagger": "2.0", "info": {"title": "a title", "version": "0.1"}
+            }
+        }}, "MySecondsRestApi": {"NotType": "AWS::ApiGateway::RestApi", "Properties": {"Body": {"swagger": "2.0", "info": {"title": "second api"}}}}}}
+        sut = cleaner.CloudFormationCleaner(self)
+        sut.clean()
+
+        assert len(self.result) == 1
+        assert "AWSTemplateFormatVersion" not in self.result[0]
+        assert "info" in self.result[0]
